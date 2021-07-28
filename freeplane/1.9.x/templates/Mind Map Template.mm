64078f4fb70bf5ace8d6336f40e27646c72f03f7
Jim Knoble <jmknoble@pobox.com>
2021-07-27 20:38:18 -0700

    Add template, add pre-commit check for *.mm files

---
+++ freeplane/1.9.x/templates/Mind Map Template.mm
@@ -0,0 +1,129 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Mind Map" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1626900726455">
+<font BOLD="false"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork" TEXT_ALIGN="DEFAULT">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Arial" SIZE="11" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" FORMAT="markdownPatternFormat"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode TEXT="Rejected" COLOR="#999999">
+<font STRIKETHROUGH="true" ITALIC="true"/>
+</stylenode>
+<stylenode TEXT="Markdown Text" FORMAT="markdownPatternFormat"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ID="ID_933583150" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="6 pt" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#666666" VGAP_QUANTITY="28 pt" MIN_WIDTH="1 cm">
+<font SIZE="18"/>
+<edge STYLE="bezier" WIDTH="7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#000000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="6 pt" SHAPE_VERTICAL_MARGIN="4 pt" BORDER_WIDTH="2 px" VGAP_QUANTITY="12 pt" MIN_WIDTH="1 cm">
+<font SIZE="14"/>
+<edge WIDTH="6"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#000000" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="4 pt" BORDER_WIDTH_LIKE_EDGE="true" BORDER_WIDTH="1 px" VGAP_QUANTITY="9 pt" MIN_WIDTH="1 cm">
+<font SIZE="13"/>
+<edge WIDTH="5"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="7 pt" MIN_WIDTH="1 cm">
+<font SIZE="12"/>
+<edge WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="6 pt" MIN_WIDTH="1 cm">
+<font SIZE="11"/>
+<edge WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="4 pt" MIN_WIDTH="1 cm">
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="4 pt" MIN_WIDTH="1 cm">
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="4 pt" MIN_WIDTH="1 cm">
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="4 pt" MIN_WIDTH="1 cm">
+<font SIZE="10"/>
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#000000" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="3 pt" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#000000" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="3 pt" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+<edge WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" COLOR="#000000" FORMAT="NO_FORMAT" BORDER_WIDTH_LIKE_EDGE="true" VGAP_QUANTITY="3 pt" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+<edge WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,12" COLOR="#000000" VGAP_QUANTITY="3 pt" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,13" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,14" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,15" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,16" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,17" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,18" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,19" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,20" COLOR="#333333" MIN_WIDTH="1 cm">
+<font SIZE="8"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="19" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+</node>
+</map>
